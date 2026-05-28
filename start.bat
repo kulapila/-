bbc08fa93a860e5955277c22dc71adf635529bc2
kurapica <yangyufei@example.com>
2026-05-28 21:14:48 +0800

    add git pull back to start.bat

--- start.bat
+++ start.bat
@@ -1,6 +1,8 @@
 @echo off
 cd /d d:\cc-test\survey_pipeline
+taskkill //f //im python.exe 2>nul
+echo Pulling latest data...
+git pull
 echo Starting viewer...
-start http://127.0.0.1:8080
 python -m src.viewer
 pause
